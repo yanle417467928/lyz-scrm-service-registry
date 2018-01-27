e2fbc9c11bb9d7cf580d4d97c43030fb12f3ae90
leyan <417467928@qq.com>
2018-01-27 22:34:57 +0800

    Initial commit

---
+++ src/main/java/cn/com/leyizhuang/lyz/scrm/service/registry/LyzScrmServiceRegistryApplication.java
@@ -0,0 +1,12 @@
+package cn.com.leyizhuang.lyz.scrm.service.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyzScrmServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyzScrmServiceRegistryApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/com/leyizhuang/lyz/scrm/service/registry/LyzScrmServiceRegistryApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.com.leyizhuang.lyz.scrm.service.registry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LyzScrmServiceRegistryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
